71bf032b9b2f2c86f0eecbb73578bf46397e0e9f
tomoyahiroe <tomoya4creative@gmail.com>
2024-04-13 15:01:16 +0900

    change style settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,14 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    minHeight: {
+      '0': '0',
+      '1/4': '25%',
+      '1/2': '50%',
+      '3/4': '75%',
+      'full': '100%',
+      'half-screen': '50vh',
+    }
   },
   plugins: [],
 };
